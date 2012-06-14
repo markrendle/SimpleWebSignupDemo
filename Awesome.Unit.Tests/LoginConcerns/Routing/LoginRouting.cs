2987d88b07bd8116fc8319eaed61f7afbabdc97c
Iain Ballard <iain.ballard@7digital.com>
2012-06-14 18:36:06 +0100

    tests around routing! :-)

---
+++ Awesome.Unit.Tests/LoginConcerns/Routing/LoginRouting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AwesomeThing.Login;
+using NUnit.Framework;
+using Simple.Web;
+using Tests.Scaffold;
+
+namespace Tests.LoginConcerns.Routing
+{
+	[Scenario]
+	class LoginRouting
+	{
+		[Then]
+		public void login_post_handled_by_PostLogin()
+		{
+			var matches = 
+				MySite()
+				.GetHandlersFor("/login")
+				.RespondingTo<IPost>();
+
+			Assert.That(matches.Single().FullName, Is.EqualTo(typeof(PostLogin).FullName)); // gets wrapped in RunTimeType :-(
+		}
+
+		[Then]
+		public void login_url_should_have_only_one_post_handler()
+		{
+			var match_count = 
+				MySite()
+				.GetHandlersFor("/login")
+				.RespondingTo<IPost>()
+				.Count();
+
+			Assert.That(match_count, Is.EqualTo(1));
+		}
+
+		IEnumerable<Assembly> MySite()
+		{
+			return AppDomain.CurrentDomain.GetAssemblies();
+		}
+	}
+}
