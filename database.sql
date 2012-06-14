df58264b63f3ccb211fa143a141290ca035d3639
Iain Ballard <iain.ballard@7digital.com>
2012-06-14 14:41:53 +0100

    hardened database script

--- database.sql
+++ database.sql
@@ -1,3 +1,15 @@
+USE [master]
+
+IF (EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE ('[' + name + ']' = 'SimpleWeb_Signup' OR name = 'SimpleWeb_Signup')))
+BEGIN
+	ALTER DATABASE [SimpleWeb_Signup] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	DROP DATABASE [SimpleWeb_Signup]
+END
+CREATE DATABASE [SimpleWeb_Signup];
+GO
+
+USE [SimpleWeb_Signup]
+
 CREATE TABLE [dbo].[Signup](
     [Id] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
     [Email] [nvarchar](256) NOT NULL,
